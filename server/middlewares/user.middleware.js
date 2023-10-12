29e86e261b18df206c5836f8062b2400a730eb0e
Mouhsine Nejmi <nejmimouhsine@gmail.com>
2023-10-12 21:32:36 +0100

    Update users data

--- server/middlewares/user.middleware.js
+++ server/middlewares/user.middleware.js
@@ -17,7 +17,8 @@ const isUserAuthenticated = (req, res, next) => {
 
     next();
   } catch (error) {
-    next(error);
+    console.log(error);
+    return res.json({ status: 500, message: error.message });
   }
 };
 
@@ -46,4 +47,32 @@ const isUserAdminOrManager = (req, res, next) => {
   }
 };
 
-module.exports = { isUserAuthenticated, isUserAdminOrManager };
+const isUserAdmin = (req, res, next) => {
+  try {
+    const authHeader = req.headers['authorization'];
+    const accessToken = authHeader && authHeader.split(' ')[1];
+
+    if (!accessToken) {
+      return res
+        .status(403)
+        .json({ message: 'Access Denied. No token provided.' });
+    }
+
+    const decoded = jwt.verify(accessToken, process.env.JWT_ACCESS_SECRET_KEY);
+
+    if (decoded.role !== 'admin' && decoded.role === 'manager') {
+      return res.status(403).json({
+        message: "Access Denied. You don't have enough priviliege.",
+      });
+    }
+
+    if (decoded.role === 'admin') {
+      next();
+    }
+  } catch (error) {
+    console.log(error);
+    return res.json({ status: 500, message: error.message });
+  }
+};
+
+module.exports = { isUserAuthenticated, isUserAdminOrManager, isUserAdmin };
