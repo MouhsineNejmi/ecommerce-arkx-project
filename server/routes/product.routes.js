64e90fedc21778b060998ff3d4056e958ca64350
Zakaria <Zakaria.ajiyalona@gmail.com>
2023-10-19 09:29:54 +0100

    updated

--- server/routes/product.routes.js
+++ server/routes/product.routes.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const { addProduct,listProduct, deleteProduct,updateProduct,getProductID,searchforProduct} = require("../controllers/Product.controller");
+const { addProduct,listProduct, deleteProduct,updateProduct,getProductID,searchforProduct} = require("../controllers/product.controller");
 
 
 router.post("/", addProduct);
