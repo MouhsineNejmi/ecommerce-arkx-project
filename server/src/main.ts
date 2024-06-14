914ea7c0b0c7470a80dcc5c9db02e38cbd253301
MouhsineNejmi <nejmimouhsine@gmail.com>
2024-06-14 14:15:54 +0100

    add swagger

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,9 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('Ecommerce Arkx')
+    .setDescription('The Ecommerce API endpoints')
+    .setVersion('0.1')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(5000);
 }
 bootstrap();
