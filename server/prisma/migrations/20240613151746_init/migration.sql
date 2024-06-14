f3b2f69fdc93eea4b3bcbbb50f13ac81039589ef
Mouhsine Nejmi <97952181+MouhsineNejmi@users.noreply.github.com>
2024-06-14 10:50:50 +0100

    Push server data (#77)

---
+++ server/prisma/migrations/20240613151746_init/migration.sql
@@ -0,0 +1,25 @@
+-- CreateEnum
+CREATE TYPE "ROLE" AS ENUM ('admin', 'seller', 'manager', 'general');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "first_name" TEXT NOT NULL,
+    "last_name" TEXT NOT NULL,
+    "username" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "role" "ROLE" NOT NULL DEFAULT 'general',
+    "profile" TEXT NOT NULL DEFAULT 'https://github.com/shadcn.png',
+    "is_email_verified" BOOLEAN NOT NULL DEFAULT false,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
