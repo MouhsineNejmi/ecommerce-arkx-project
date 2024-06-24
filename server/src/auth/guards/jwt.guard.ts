c2284bbdd7e51551f6ba1ee3984d061c0c9fdf1a
MouhsineNejmi <nejmimouhsine@gmail.com>
2024-06-24 17:05:30 +0100

    implement auth guard

--- server/src/auth/guards/jwt.guard.ts
+++ server/src/auth/guards/jwt.guard.ts
@@ -1,21 +1,21 @@
 import { ExecutionContext, Injectable } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import { AuthGuard } from '@nestjs/passport';
+import { Observable } from 'rxjs';
 
 @Injectable()
 export class JwtGuard extends AuthGuard('jwt') {
   constructor(private reflector: Reflector) {
     super();
   }
-
-  canActivate(context: ExecutionContext) {
+  canActivate(
+    context: ExecutionContext,
+  ): Promise<boolean> | boolean | Observable<boolean> {
     const isPublic = this.reflector.getAllAndOverride('isPublic', [
       context.getHandler(),
       context.getClass(),
     ]);
-
     if (isPublic) return true;
-
     return super.canActivate(context);
   }
 }
