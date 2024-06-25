fe76879ab98a3fe9b60c4c41edf1447c65352e65
MouhsineNejmi <nejmimouhsine@gmail.com>
2024-06-25 12:34:55 +0100

    update usermenu in navbar

--- client/src/components/office/navbar.tsx
+++ client/src/components/office/navbar.tsx
@@ -1,24 +1,19 @@
-"use client";
-
-import { useSession } from "next-auth/react";
-
 import UserMenu from "@/components/shared/user-menu";
-import StoreSwitcher from "@/components/office/store-switcher";
-// import { ThemeToggle } from '@/components/theme-toggle';
+// import { ThemeToggle } from "@/components/theme-toggle";
 
-import { useUserStores } from "@/hooks/use-user-stores";
+import { Session } from "next-auth";
 
-const Navbar = () => {
-  const { data: session } = useSession();
-  const { stores, loading } = useUserStores();
+interface NavbarProps {
+  currentUser: Session["user"];
+}
 
+const Navbar = ({ currentUser }: NavbarProps) => {
   return (
     <div className="border-b md:ml-60">
       <div className="flex items-center h-16 px-4">
-        <StoreSwitcher items={stores} loading={loading} />
         <div className="flex items-center ml-auto space-x-4">
           {/* <ThemeToggle /> */}
-          <UserMenu currentUser={session?.user} />
+          <UserMenu currentUser={currentUser} />
         </div>
       </div>
     </div>
